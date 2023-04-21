250ac61a3b780bdf947b63ec09a734981253d4b7
Tracy Matta <tracygmatta@gmail.com>
2023-04-22 00:42:52 +0300

    fixed names

--- src/main/java/com/example/expensetracker/HelloApplication.java
+++ src/main/java/com/example/expensetracker/HelloApplication.java
@@ -7,11 +7,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/expensetracker/home.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("home.fxml")));
         Scene scene = new Scene(root);
         stage.setTitle("home");
         stage.setScene(scene);
