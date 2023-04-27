fb401aed78c7fb097447205eff8228f0179e3de9
TaniaZaitouny <taniazaiitouny@gmail.com>
2023-04-27 13:30:34 +0300

    pages

--- src/main/java/com/example/expensetracker/HelloApplication.java
+++ src/main/java/com/example/expensetracker/HelloApplication.java
@@ -1,9 +1,13 @@
 package com.example.expensetracker;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import java.sql.*;
 
@@ -21,7 +25,26 @@ public class HelloApplication extends Application {
         stage.setTitle("home");
         stage.setScene(scene);
         stage.show();
+        // Get a reference to the VBox element in the home.fxml file
+        VBox vbox = (VBox)scene.lookup("#home_category");
 
+        // Add the mouse entered event listener to make the VBox glow
+        vbox.setOnMouseEntered(e -> {
+            DropShadow dropShadow = new DropShadow();
+            dropShadow.setRadius(15);
+            dropShadow.setSpread(0.5);
+            dropShadow.setColor(Color.LAVENDER);
+            vbox.setEffect(dropShadow);
+            vbox.setScaleX(1.1);
+            vbox.setScaleY(1.1);
+        });
+
+        // Add the mouse exited event listener to revert the VBox back to its original style
+        vbox.setOnMouseExited(e -> {
+            vbox.setEffect(null);
+            vbox.setScaleX(1.0);
+            vbox.setScaleY(1.0);
+        });
         DatabaseConnection db = DatabaseConnection.getInstance();
         Connection connection = db.getConnection();
     }
