f9983d2cbffd92bf4e0e9b8a71d390e316385ae5
Tracy Matta <tracygmatta@gmail.com>
2023-04-27 20:46:57 +0300

    configured charts and added common layout

--- src/main/java/com/example/expensetracker/Controllers/HomeController.java
+++ src/main/java/com/example/expensetracker/Controllers/HomeController.java
@@ -1,19 +1,5 @@
 package com.example.expensetracker.Controllers;
 
-import com.example.expensetracker.HelloApplication;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.util.Objects;
-
 public class HomeController {
 
 
--- src/main/java/com/example/expensetracker/Controllers/ReportsController.java
+++ src/main/java/com/example/expensetracker/Controllers/ReportsController.java
@@ -1,29 +1,13 @@
 package com.example.expensetracker.Controllers;
 
-import com.example.expensetracker.Database.DatabaseConnection;
-import com.example.expensetracker.HelloApplication;
 import com.example.expensetracker.Models.Transaction;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.chart.*;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
 import javafx.util.Pair;
 
-import java.io.IOException;
-import java.net.URL;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.ResourceBundle;
 
 public class ReportsController
 {
