4e46b354223741d7ea14fe652d8f62f49d01a890
Tracy Matta <tracygmatta@gmail.com>
2023-04-29 00:59:53 +0300

    added icons to categories

--- src/main/java/com/example/expensetracker/Controllers/CategoryController.java
+++ src/main/java/com/example/expensetracker/Controllers/CategoryController.java
@@ -1,21 +1,36 @@
 package com.example.expensetracker.Controllers;
 
+import com.example.expensetracker.HelloApplication;
 import com.example.expensetracker.Models.Category;
 import javafx.collections.FXCollections;
+import java.util.Collection;
+
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.util.Pair;
+import javafx.util.StringConverter;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class CategoryController
 {
-
     @FXML
     Button homeButton;
     @FXML
@@ -34,10 +49,16 @@ public class CategoryController
     Button submitButton;
     @FXML
     TextField categoryName;
-    @FXML private TableView<Pair<String,String>> categoriesTable;
-    @FXML private TableColumn<Pair<String,String>, String> categoryColumn;
-    @FXML private TableColumn<Pair<String,String>,String> typeColumn;
-    @FXML private TableColumn<Pair<String,String>, String> actionsColumn;
+    @FXML
+    ComboBox<String> iconComboBox;
+    @FXML
+    TableView<Pair<String,String>> categoriesTable;
+    @FXML
+    TableColumn<Pair<String,String>, String> categoryColumn;
+    @FXML
+    TableColumn<Pair<String,String>,String> typeColumn;
+    @FXML
+    TableColumn<Pair<String,String>, String> actionsColumn;
 
 //    @FXML
 //    Label messageText;
@@ -47,6 +68,38 @@ public class CategoryController
     public void initialize() throws SQLException {
         if(categoriesTable != null) {
             getCategories();
+        }
+        else {
+            final File folder = new File("src/main/resources/com/example/expensetracker/Media");
+            final File[] files = folder.listFiles();
+            String fileName, nameWithoutExtension;
+            List<String> iconNames = new ArrayList<>();
+            int extensionIndex;
+            for (File file : files) {
+                fileName = file.getName();
+                extensionIndex = fileName.lastIndexOf(".");
+                nameWithoutExtension = extensionIndex == -1 ? fileName : fileName.substring(0, extensionIndex);
+                iconNames.add(nameWithoutExtension);
+            }
+            iconComboBox.setItems(FXCollections.observableArrayList(iconNames));
+            iconComboBox.setCellFactory(listView -> new ListCell<>() {
+                private final ImageView imageView = new ImageView();
+                @Override
+                protected void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty || item == null) {
+                        setText(null);
+                        setGraphic(null);
+                    } else {
+                        setText(item);
+                        Image image = new Image(Objects.requireNonNull(HelloApplication.class.getResourceAsStream("Media/" + item + ".png")));
+                        imageView.setImage(image);
+                        imageView.setFitWidth(32);
+                        imageView.setFitHeight(32);
+                        setGraphic(imageView);
+                    }
+                }
+            });
         }
     }
 
@@ -90,5 +143,5 @@ public class CategoryController
         categoriesTable.setItems(FXCollections.observableArrayList(categories));
     }
 
-
 }
+
